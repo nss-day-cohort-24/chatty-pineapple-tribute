c9f1130f3541aa371b0cb2b1f3a7d2e359a5a300
Sam Kimball <jibequan@gmail.com>
2018-02-07 15:49:38 -0600

    Added input and nav field

---
+++ js/input.js
@@ -0,0 +1,15 @@
+//This module will take the users input and will return the message after a key press
+
+"use strict";
+
+
+
+
+
+
+
+
+
+
+
+module.exports = ;
--- js/two.js
+++ js/two.js
@@ -1,4 +1,4 @@
-//This module will add the delete button to the individual messages.
+//This module will add the delete button to the individual messages and add them to them chat window.
 
 "use strict";
 
