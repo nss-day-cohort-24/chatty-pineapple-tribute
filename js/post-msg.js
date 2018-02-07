1de86f36bdbf1aa64f5ed0a2be868efeb1d7f7c9
Jesie Oldenburg <kitglo46@gmail.com>
2018-02-07 15:53:35 -0600

    commiting comments to files

---
+++ js/post-msg.js
@@ -0,0 +1,17 @@
+//This module will delete individual messages.
+
+// This module should contain a function that accepts an element id, and the user message, and then add the user's message - along with the delete button - to the specified parent element. Each message should be stored in a private array in this module. This module should also expose a function to read all messages, and delete a single message.
+
+// This module should accept a message element id and then remove the correct element from the DOM. This module should also remove the corresponding message from the private array that was created in the previous module.
+
+"use strict";
+
+
+
+
+
+
+
+
+
+module.exports = ;
