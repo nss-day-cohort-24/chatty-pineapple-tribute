21618daaad69435205c79f7ae7ab61503c772701
Dylan B <giglett21@gmail.com>
2018-02-08 07:32:03 -0800

    new json messages and some more js in main

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ function showData(taco) {
     let item;
     for (item in taco){
         let messageItem = taco[item];
-          messageJson += `<div><h2> ${messageItem.message}</h2></div>`
+          messageJson += `<div><h2> ${messageItem.user}:${messageItem.message}</h2></div>`
     };
 
    messageDiv.innerHTML = messageJson;
