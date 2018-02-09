e43c55ddaee344b603dbfa4b12054b3b7db42dda
Dylan B <giglett21@gmail.com>
2018-02-09 12:59:39 -0800

    fix

--- js/post-msg.js
+++ js/post-msg.js
@@ -1,50 +1,36 @@
-
-// "use strict";
-// var chatLog = document.getElementById("chatlog");
-// console.log("what is theElement?", chatLog);
-
-
-// // let userMessageInput = document.getElementById("user-msg");
-
-// // Function expression for the el....
-// var newMessageLine = function sentIt() {
-//     var userMessageInput = document.getElementById("user-msg");
-//     console.log("what is userMessageInput?", userMessageInput);
-//     var printValue = chatLog;
-//     console.log("what is user msg?", printValue);
-//     // console.log("what is the parent element", parentElement);
-//     printValue.innerHTML += userMessageInput;
-// };
-
-
-// var addDelButton = function createDelButton() {
-//     var deleteButton = document.createElement("BUTTON"); // Create a <button> element
-//     var textNode = document.createTextNode("Delete Line"); // Create a text node
-//     deleteButton.appendChild(t); // Append the text to <button>
-//     document.chatLog.appendChild(btn); // Append <button> to <body>;
-// };
-
-
-// var userMessageInput = document.getElementById("user-msg");
-
-// userMessageInput.addEventListener('keypress', function(e) {
-//     if (e.keyCode === 13) {
-//         var userString = document.getElementById("user-msg").value;
-//         console.log("what did they type?", userString);
-//         console.log("YOUR KEY IS WORKING!!!");
-//         // newMessageLine();
-//     }
-// });
 "use strict";
+var chatLog = document.getElementById("chatlog");
+console.log("what is theElement?", chatLog);
 
-// may need to change the require....
-const samsMod = require('./getMessages');
 
-function newLine(array) {
-    var chatBox = document.getElementById("chatbox");
-    var printValue = samsMod.pop();
-    var postTheNewLine = `<div><p>${printValue} <button class="del-line">Delete</button></p></div>`;
-    chatBox.innerHTML += postTheNewLine;
-}
+// let userMessageInput = document.getElementById("user-msg");
 
-module.exports = newLine;
+// Function expression for the el....
+var newMessageLine = function sentIt() {
+    var userMessageInput = document.getElementById("user-msg");
+    console.log("what is userMessageInput?", userMessageInput);
+    var printValue = chatLog;
+    console.log("what is user msg?", printValue);
+    // console.log("what is the parent element", parentElement);
+    printValue.innerHTML += userMessageInput;
+};
+
+
+var addDelButton = function createDelButton() {
+    var deleteButton = document.createElement("BUTTON"); // Create a <button> element
+    var textNode = document.createTextNode("Delete Line"); // Create a text node
+    deleteButton.appendChild(t); // Append the text to <button>
+    document.chatLog.appendChild(btn); // Append <button> to <body>;
+};
+
+
+var userMessageInput = document.getElementById("user-msg");
+
+userMessageInput.addEventListener('keypress', function(e) {
+    if (e.keyCode === 13) {
+        var userString = document.getElementById("user-msg").value;
+        console.log("what did they type?", userString);
+        console.log("YOUR KEY IS WORKING!!!");
+        // newMessageLine();
+    }
+});
